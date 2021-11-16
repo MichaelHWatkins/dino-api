41f96bd1557fccba1aafc095a211ef59cb266abb
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-11-16 14:26:38 -0800

    made the first promise commit...
    Co-Authored-by: Matt Luker <jamesmattluker@gmail.com>

--- src/index.js
+++ src/index.js
@@ -7,46 +7,50 @@ $(document).ready(function() {
   $('#weatherLocation').click(function() {
     const city = $('#location').val();
     $('#location').val("");
-
-    let request = new XMLHttpRequest();
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${city}&limit=25&offset=0&rating=g&lang=en`;
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        const giphyURL = response.data[0].images.fixed_height.url;
-        getElements(giphyURL);
-      }
-    };
-
-    request.open("GET", url, true);
-    request.send();
-
-   function getElements(giphyURL) {
-      $('#searchResults').attr('src', giphyURL);
-    }
-  });
-
-  $('#random').click(function() {
-    const city = $('#location').val();
-    $('#location').val("");
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${city}&limit=25&offset=0&rating=g&lang=en`;
+      request.onload = function() {      
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
     
+
+      request.open("GET", url, true);
+      request.send();
+    });
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('#searchResults').attr('src', body.data[0].images.fixed_height.url);
+    }, function(error) {
+      $('#searchResults').attr(`There was an error processing your request: ${error}`);
+    });
+  });
+});
+
+$('#random').click(function() {
+  
+  let promise = new Promise(function(resolve, reject) {
     let request = new XMLHttpRequest();
     const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&q=&limit=25&offset=0&rating=g&lang=en`;
 
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        const giphyURL1 = response.data.images.fixed_height.url;
-        getElements(giphyURL1);
-      }
+    request.onload = function() {
+      if (this.status === 200) {
+        resolve(request.response);
+      } else {
+        reject(request.response);
+      }      
     };
 
     request.open("GET", url, true);
     request.send();
-
-    function getElements(giphyURL1) {
-      $('#randomResult').attr('src', giphyURL1);
-    }
+  });
+  promise.then(function(response) {
+    const body = JSON.parse(response);
+    console.log(body.data)
+    $('#randomResult').attr('src', body.data.images.fixed_height.url);    
   });
 });
