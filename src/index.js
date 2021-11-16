a2541833155225e8bd1f609bb4ff3848902edfdd
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-11-16 14:57:55 -0800

    made the service files...
    Co-Authored-by: Matt Luker <jamesmattluker@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,26 +2,15 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import SearchService from './search-service.js'
+import RandomService from './random-service.js'
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     const city = $('#location').val();
     $('#location').val("");
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${city}&limit=25&offset=0&rating=g&lang=en`;
-      request.onload = function() {      
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(request.response);
-        }
-      };
     
-
-      request.open("GET", url, true);
-      request.send();
-    });
+    let promise = SearchService.getSearch(city);
     promise.then(function(response) {
       const body = JSON.parse(response);
       $('#searchResults').attr('src', body.data[0].images.fixed_height.url);
@@ -33,21 +22,7 @@ $(document).ready(function() {
 
 $('#random').click(function() {
   
-  let promise = new Promise(function(resolve, reject) {
-    let request = new XMLHttpRequest();
-    const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&q=&limit=25&offset=0&rating=g&lang=en`;
-
-    request.onload = function() {
-      if (this.status === 200) {
-        resolve(request.response);
-      } else {
-        reject(request.response);
-      }      
-    };
-
-    request.open("GET", url, true);
-    request.send();
-  });
+  let promise = RandomService.getRandom();
   promise.then(function(response) {
     const body = JSON.parse(response);
     console.log(body.data)
